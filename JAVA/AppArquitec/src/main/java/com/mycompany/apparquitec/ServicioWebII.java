261b224b3767e5d2a83c074bcaba659d485f70cb
Andrea Polanco <alpm9615@gmail.com>
2017-04-03 00:16:21 -0400

    Se realizo cambio en el segundo metodod

--- JAVA/AppArquitec/src/main/java/com/mycompany/apparquitec/ServicioWebII.java
+++ JAVA/AppArquitec/src/main/java/com/mycompany/apparquitec/ServicioWebII.java
@@ -5,6 +5,7 @@
  */
 package com.mycompany.apparquitec;
 
+import com.arquitectura.models.Peticion;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -20,17 +21,19 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "ServicioWebII", urlPatterns = {"/ServicioWebII"})
 public class ServicioWebII extends HttpServlet {
 
-    protected void isAlive(HttpServletRequest request, HttpServletResponse response)
+    protected void procces(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-
-        String p1 = "p1";
-        String p2 = "p2";
-        String value1 = request.getParameter(p1);
-        String value2 = request.getParameter(p2);
-        int n1 = Integer.parseInt(value1);
-        int n2 = Integer.parseInt(value2);
+        Peticion p = new Peticion();
+        p.setName(request.getParameter("name"));
+        p.setLastName(request.getParameter("lastName"));
+        p.setEmail(request.getParameter("email"));
+        p.setPhoneNumber(request.getParameter("phoneNumber"));
+        p.setId(request.getParameter("id"));
+        p.setNumb1(Integer.parseInt(request.getParameter("p1")));
+        p.setNumb2(Integer.parseInt(request.getParameter("p2")));
+      
 
         out.println("<html>");
         out.println("<head>");
@@ -38,16 +41,30 @@ public class ServicioWebII extends HttpServlet {
         out.println("</head>");
         out.println("<body bgcolor=\"white\">");
         out.println("<h1>");
-         out.write("Aqui el resultado:");
+         out.write("Suma :");
         out.write("<br>");
-        out.write(p1);
-        out.write(" ( " + n1 + " )");
+        out.write("p1");
+        out.write(" ( " + p.getNumb1() + " )");
         out.write(" + ");
-        out.write(p2);
-        out.write(" ( " + n2 + " )");
+        out.write("p2");
+        out.write(" ( " + p.getNumb2() + " )");
         out.write(" = ");
-        out.write((n1 + n2) + "");
+        out.write((p.getNumb1() + p.getNumb2()) + "");
         out.println("</h1>");
+        
+        out.write("<h2>Datos de la persona: ");
+         out.write("<br>");
+        out.write("Nombre: " + p.getName());
+         out.write("<br>");
+        out.write("Apellido: " + p.getLastName());
+         out.write("<br>");
+        out.write("Id: "+ p.getId());
+         out.write("<br>");
+        out.write("Email: " + p.getEmail());
+         out.write("<br>");
+        out.write("Numero de telefono: "+ p.getPhoneNumber());
+        out.write("<br>");
+        out.write("</h2>");
         out.println("</body>");
         out.println("</html>");
 
@@ -59,13 +76,13 @@ public class ServicioWebII extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        isAlive(request, response);
+        procces(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        isAlive(request, response);
+        procces(request, response);
     }
 
 }
